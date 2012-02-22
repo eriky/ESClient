3c7af6496b84a247dbe566dbf3d7e85af643b510
Erik-Jan <erik@van-baaren.nl>
2012-02-22 23:26:13 +0100

    added version and author stuff to __init__.py

---
+++ esclient/__init__.py
@@ -0,0 +1,10 @@
+#import logging
+#logger = logging.getLogger(__name__)
+
+VERSION = (0, 0, 1)
+
+__version__ = ".".join(map(str, VERSION))
+__author__ = "Erik-Jan van Baaren"
+__contact__ = "erikjan@gmail.com"
+__homepage__ = "https://github.com/eriky/ESClient/"
+__docformat__ = "restructuredtext"
