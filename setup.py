760cbabc4659a26de83d6e337f8ecc027745250d
Erik-Jan <erik@van-baaren.nl>
2012-02-27 10:03:11 +0100

    improved setup.py to include complete readme as description

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@
 from distutils.core import setup
 import esclient
 
+with open('README.rst') as file:
+    long_description = file.read()
+    
 setup(name='ESClient',
         version=esclient.get_version(),
         description='A lightweight Python client for ElasticSearch',
@@ -21,7 +24,5 @@ setup(name='ESClient',
             'Programming Language :: Python',
             'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
             ],
-        long_description = """\
-ESClient is a Python library that uses the ElasticSearch REST API. It is meant
-to be lightweight and be close to the actual REST API in terms of usage."""
+        long_description = long_description
         )
